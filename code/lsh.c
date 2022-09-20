bb4bd374d566c39fd4fc5dc29554fbfea873cdc0
Rakarake <williambodin2001@gmail.com>
2022-09-20 22:09:35 +0200

    Piping works

--- code/lsh.c
+++ code/lsh.c
@@ -31,11 +31,15 @@
 #define TRUE 1
 #define FALSE 0
 
+#define READ_END 0
+#define WRITE_END 1
+
 void RunCommand(int, Command *);
 void DebugPrintCommand(int, Command *);
 void PrintPgm(Pgm *);
 void stripwhite(char *);
 
+void process_pgm();
 void printenv();
 
 extern char **environ;
@@ -81,45 +85,131 @@ int main(void)
  * 1. Implement this function so that it executes the given command(s).
  * 2. Remove the debug printing before the final submission.
  */
-void RunCommand(int parse_result, Command *cmd)
-{
-  Pgm *current_pgm = cmd->pgm;
-  while (current_pgm != NULL) {
-    char *pname = current_pgm->pgmlist[0];
-    char **pname_args = current_pgm->pgmlist;
-    char **pargs = current_pgm->pgmlist + 1;
 
-    //First, handle special commands (exit, cd)
-    if (!strcmp(pname, "exit")) {
-      printf("goodbye! 👋");
-      exit(0);
-    } else if (!strcmp(pname, "cd")) {
-      chdir(pargs[0]);
-      return;
-    }
-
-    // Create process
-    int pid;
-    DebugPrintCommand(parse_result, cmd);
-    pid = fork();
-    if (pid == -1) {
-      printf("could not fork current process\n");
-      return;
-    }
-    if (pid == 0) {
-      // Child process
-      //execlp(pname, pname, NULL);
-      execvp(pname, pname_args);
-    } else {
-      // Parent prosess (shell)
-      wait(NULL);
-    }
-
-    // TODO: make this process pipe output to next process' input
-    // Next command
-    current_pgm = current_pgm->next;
+void RunCommand(int parse_result, Command *cmd) {
+  DebugPrintCommand(parse_result, cmd);
+  int pid = fork();
+  if (pid == 0) {
+    // Child process (fork)
+    process_pgm(cmd->pgm);
+  } else {
+    // Parent process (shell)
+    wait(NULL);
   }
 }
+
+void process_pgm(Pgm *pgm) {
+  char *pname =       pgm->pgmlist[0];
+  char **pname_args = pgm->pgmlist;
+  char **pargs =      pgm->pgmlist + 1;
+
+  // Shell commands (exit, cd)
+  if (!strcmp(pname, "exit")) {
+    printf("goodbye! 👋");
+    exit(0);
+  } else if (!strcmp(pname, "cd")) {
+    chdir(pargs[0]);
+    return;
+  }
+
+  if (pgm->next != NULL) {
+    // Create pipe, then fork
+    int pipe_fd[2];
+
+    if (pipe(pipe_fd) == -1) {
+      fprintf(stderr,"Pipe failed");
+      exit(1);
+    }
+
+    int ppid = fork();
+    if (ppid == -1) {
+      printf("pipe-fork failed!\n");
+      exit(1);
+    }
+
+    if (ppid == 0) {
+      // The end who wants to write (the child)
+      close(pipe_fd[READ_END]);
+      // Set the output stream to the pipe write end
+      dup2(pipe_fd[WRITE_END], 1);
+      process_pgm(pgm->next);
+    } else {
+      // The end who wants to read (the parent)
+      close(pipe_fd[WRITE_END]);
+      // Set the input stream to the pipe read end
+      dup2(pipe_fd[READ_END], 0);
+      // Execute the reading process process
+      execvp(pname, pname_args);
+    }
+  } else {
+    execvp(pname, pname_args);
+  }
+}
+
+//void RunCommand(int parse_result, Command *cmd)
+//{
+//  Pgm *current_pgm = cmd->pgm;
+//  while (current_pgm != NULL) {
+//    char *pname = current_pgm->pgmlist[0];
+//    char **pname_args = current_pgm->pgmlist;
+//    char **pargs = current_pgm->pgmlist + 1;
+//
+//    //First, handle special commands (exit, cd)
+//    if (!strcmp(pname, "exit")) {
+//      printf("goodbye! 👋");
+//      exit(0);
+//    } else if (!strcmp(pname, "cd")) {
+//      chdir(pargs[0]);
+//      return;
+//    }
+//
+//    // Create process
+//    int pid;
+//    DebugPrintCommand(parse_result, cmd);
+//    pid = fork();
+//    if (pid == -1) {
+//      printf("could not fork current process!\n");
+//      exit(1);
+//    }
+//    if (pid == 0) {
+//      // If other elements in linked-list, create pipe and fork again
+//      if (current_pgm->next != NULL) {
+//        int pipe_fd[2];
+//
+//        if (pipe(pipe_fd) == -1) {
+//          fprintf(stderr,"Pipe failed");
+//          exit(1);
+//        }
+//
+//        int ppid = fork();
+//        if (ppid == -1) {
+//          printf("pipe-fork failed!\n");
+//          exit(1);
+//        }
+//
+//        if (ppid == 0) {
+//          // The end who wants to write (the child)
+//          close(pipe_fd[READ_END]);
+//          // Set the output stream to the pipe write end
+//          dup2(pipe_fd[WRITE_END], 1);
+//        } else {
+//          // The end who wants to read (the parent)
+//          close(pipe_fd[WRITE_END]);
+//          // Set the input stream to the pipe read end
+//          dup2(pipe_fd[READ_END], 0);
+//          // Execute the reading process process
+//          execvp(pname, pname_args);
+//        }
+//      }
+//    } else {
+//      // Parent prosess (shell)
+//      wait(NULL);
+//    }
+//
+//    // Next command
+//    current_pgm = current_pgm->next;
+//  }
+//}
 
 
 /* 

b70ea79de9fba698f88644af1d3a2c1d9f23b6f8
Rakarake <rak@rakarake.xyz>
2024-03-18 16:54:41 +0100

    Flake instead of shell

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Sample project";
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        deps = with pkgs; [
+          readline
+        ];
+      in
+      {
+        #defaultPackage = pkgs.stdenv.mkDerivation {
+        #  name = "lsh";
+        #  src = ./.;
+        #  buildInputs = deps; 
+        #  buildPhase = ''
+        #    gcc -o lsh main.c $(pkg-config --cflags --libs sdl2)
+        #  '';
+        #  installPhase = ''
+        #    mkdir -p $out/bin
+        #    cp sdl2-test $out/bin
+        #  '';
+        #};
+        devShell = pkgs.mkShell {
+          buildInputs = deps;
+        };
+      }
+    );
+}
+
